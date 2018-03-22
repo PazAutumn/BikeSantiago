0bf20e991379c47a7ef2399ee4e62d44c596a85a
AriellaRogueN <arrodriguezn@gmail.com>
2018-03-22 10:52:11 -0300

    Solucionando Conflictos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import BikeMap from './components/BikeMap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
+        <MuiThemeProvider>
+          <Grid>
+            <Row>
+              <Col xs={12}>
+                <BikeMap />
+              </Col>
+            </Row>
+          </Grid>
+        </MuiThemeProvider>
       </div>
     );
   }
